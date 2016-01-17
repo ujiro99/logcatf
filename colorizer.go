8964a34021f31431c94e49cfc8486b81b8dddcc1
ujiro99 <siro.cola@gmail.com>
2016-01-17 16:10:34 +0900

    expand color map for performance
    benchmark                                   old ns/op   new ns/op   delta
    BenchmarkDefault-4                          83436       71472       -14.34%
    BenchmarkDefault_initialize-4               84744       67474       -20.38%
    BenchmarkDefault_initialize_colorizer-4     13412       1115        -91.69%
    BenchmarkDefault_initialize_formatter-4     9006        9138        + 1.47%
    BenchmarkDefault_initialize_writer-4        5.21        5.32        + 2.11%
    BenchmarkDefault_parseLine-4                27044       27807       + 2.82%
    BenchmarkDefault_execute-4                  262         270         + 3.05%
    BenchmarkDefault_execute_empty-4            11.7        14.5        +23.93%
    BenchmarkEncodeShiftJis-4                   88912       78185       -12.06%
    BenchmarkToCsv-4                            79118       65068       -17.76%
    BenchmarkExecCommand-4                      108333      99946       - 7.74%
    BenchmarkColor-4                            97009       84273       -13.13%
    BenchmarkParse-4                            12255       12548       + 2.39%
    BenchmarkParse_FindFormat-4                 6374        7403        +16.14%
    BenchmarkParse_Search-4                     5990        7755        +29.47%
    
    benchmark                                   old allocs  new allocs  delta
    BenchmarkDefault-4                          221         171         -22.62%
    BenchmarkDefault_initialize-4               259         209         -19.31%
    BenchmarkDefault_initialize_colorizer-4     51          2           -96.08%
    BenchmarkDefault_initialize_formatter-4     3           3           + 0.00%
    BenchmarkDefault_initialize_writer-4        0           0           + 0.00%
    BenchmarkDefault_parseLine-4                63          63          + 0.00%
    BenchmarkDefault_execute-4                  0           0           + 0.00%
    BenchmarkDefault_execute_empty-4            0           0           + 0.00%
    BenchmarkEncodeShiftJis-4                   233         183         -21.46%
    BenchmarkToCsv-4                            233         183         -21.46%
    BenchmarkExecCommand-4                      275         225         -18.18%
    BenchmarkColor-4                            233         183         -21.46%
    BenchmarkParse-4                            4           4           + 0.00%
    BenchmarkParse_FindFormat-4                 0           0           + 0.00%
    BenchmarkParse_Search-4                     4           4           + 0.00%
    
    benchmark                                   old bytes   new bytes   delta
    BenchmarkDefault-4                          62428       56551       - 9.41%
    BenchmarkDefault_initialize-4               60580       54701       - 9.70%
    BenchmarkDefault_initialize_colorizer-4     6215        336         -94.59%
    BenchmarkDefault_initialize_formatter-4     40          40          + 0.00%
    BenchmarkDefault_initialize_writer-4        0           0           + 0.00%
    BenchmarkDefault_parseLine-4                2635        2635        + 0.00%
    BenchmarkDefault_execute-4                  0           0           + 0.00%
    BenchmarkDefault_execute_empty-4            0           0           + 0.00%
    BenchmarkEncodeShiftJis-4                   71326       65446       - 8.24%
    BenchmarkToCsv-4                            67297       61413       - 8.74%
    BenchmarkExecCommand-4                      67451       61567       - 8.72%
    BenchmarkColor-4                            63297       57413       - 9.30%
    BenchmarkParse-4                            560         560         + 0.00%
    BenchmarkParse_FindFormat-4                 0           0           + 0.00%
    BenchmarkParse_Search-4                     560         560         + 0.00%

--- colorizer.go
+++ colorizer.go
@@ -69,6 +69,61 @@ var defaultColors = [][]string{
 	{"reset_bold", "21"},
 }
 
+var colorMap = map[string]string{
+	// Default foreground/background colors
+	prefix + "default":   "39",
+	prefix + "_default_": "49",
+
+	// Foreground colors
+	prefix + "black":         "30",
+	prefix + "red":           "31",
+	prefix + "green":         "32",
+	prefix + "yellow":        "33",
+	prefix + "blue":          "34",
+	prefix + "magenta":       "35",
+	prefix + "cyan":          "36",
+	prefix + "light_gray":    "37",
+	prefix + "dark_gray":     "90",
+	prefix + "light_red":     "91",
+	prefix + "light_green":   "92",
+	prefix + "light_yellow":  "93",
+	prefix + "light_blue":    "94",
+	prefix + "light_magenta": "95",
+	prefix + "light_cyan":    "96",
+	prefix + "white":         "97",
+
+	// Background colors
+	prefix + "_black_":         "40",
+	prefix + "_red_":           "41",
+	prefix + "_green_":         "42",
+	prefix + "_yellow_":        "43",
+	prefix + "_blue_":          "44",
+	prefix + "_magenta_":       "45",
+	prefix + "_cyan_":          "46",
+	prefix + "_light_gray_":    "47",
+	prefix + "_dark_gray_":     "100",
+	prefix + "_light_red_":     "101",
+	prefix + "_light_green_":   "102",
+	prefix + "_light_yellow_":  "103",
+	prefix + "_light_blue_":    "104",
+	prefix + "_light_magenta_": "105",
+	prefix + "_light_cyan_":    "106",
+	prefix + "_white_":         "107",
+
+	// Attributes
+	prefix + "bold":       "1",
+	prefix + "dim":        "2",
+	prefix + "underline":  "4",
+	prefix + "blink_slow": "5",
+	prefix + "blink_fast": "6",
+	prefix + "invert":     "7",
+	prefix + "hidden":     "8",
+
+	// Reset to reset everything to their defaults
+	prefix + "reset":      "0",
+	prefix + "reset_bold": "21",
+}
+
 // priorityColors represents default color of priority.
 var priorityColors = map[string]string{
 	"V": "default",
@@ -107,21 +162,11 @@ func (f *Colorizer) Init(colorEnable bool, config ColorConfig) {
 	}
 
 	c := colorstring.Colorize{
-		Colors:  f.generateColorMap(),
+		Colors:  colorMap,
 		Disable: !colorEnable,
 		Reset:   true,
 	}
 	f.context = c
-}
-
-// generate a safety color code.
-func (f *Colorizer) generateColorMap() map[string]string {
-	colorMap := map[string]string{}
-	for _, pair := range defaultColors {
-		newKey := prefix + pair[defaultColorsKey]
-		colorMap[newKey] = pair[defaultColorsValue]
-	}
-	return colorMap
 }
 
 // ReplaceColorCode replaces colorCode in format, to be safety.
