d21ffe5ca76b0f69586f29971691c218f3ff6c59
ujiro99 <siro.cola@gmail.com>
2016-01-30 12:01:58 +0900

    use build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}" -os="windows darwin linux"
+cd dist
+mv  darwin_386_logcatf        logcatf
+zip darwin_386_logcatf        logcatf     -qm
+mv  darwin_amd64_logcatf      logcatf
+zip darwin_amd64_logcatf      logcatf     -qm
+mv  linux_386_logcatf         logcatf
+zip linux_386_logcatf         logcatf     -qm
+mv  linux_amd64_logcatf       logcatf
+zip linux_amd64_logcatf       logcatf     -qm
+mv  linux_arm_logcatf         logcatf
+zip linux_arm_logcatf         logcatf     -qm
+mv  windows_386_logcatf.exe   logcatf.exe
+zip windows_386_logcatf       logcatf.exe -qm
+mv  windows_amd64_logcatf.exe logcatf.exe
+zip windows_amd64_logcatf     logcatf.exe -qm
+cd -
